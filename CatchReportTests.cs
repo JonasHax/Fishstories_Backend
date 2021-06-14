c3f4ab043757c3bea94d6cba454743e8b59b37a0
JonasHax <33658599+JonasHax@users.noreply.github.com>
2021-06-14 12:52:51 +0200

    Add files via upload

---
+++ CatchReportTests.cs
@@ -0,0 +1,67 @@
+﻿using FiskeNettet;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DatabaseTests
+{
+    public class CatchReportTests
+    {
+        private readonly HttpClient _client;
+
+        public CatchReportTests()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            _client = appFactory.CreateClient();
+        }
+
+        [Fact]
+        public async Task TestGetAllCatchReport()
+        {
+            //ARRANGE
+            var request = "api/CatchReport";
+
+            //ACT
+            //Check if httpResponse is 200 / OK
+            var response = await _client.GetAsync(request);
+
+            //ASSERT
+            response.EnsureSuccessStatusCode();
+
+        }
+
+        [Fact]
+        public async Task TestGetSingleCatchReport()
+        {
+            //ARRANGE
+            var request = "api/CatchReport/608fa4c94d7a28268733aab0";
+
+            //ACT
+            //Check if httpResponse is 200 / OK
+            var response = await _client.GetAsync(request);
+
+            //ASSERT
+            response.EnsureSuccessStatusCode();
+
+        }
+
+        [Fact]
+        public async Task TestRemoveCatchReport()
+        {
+            //ARRANGE
+
+            var request = "api/CatchReport/60ae3ff31d418bca0b8f975e";
+
+            //ACT
+            //Check if httpResponse is 200 / OK
+            var response = await _client.DeleteAsync(request);
+
+            //ASSERT
+            response.EnsureSuccessStatusCode();
+
+        }
+
+
+    }
+}
